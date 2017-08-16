91e477a81b0df51c8f1e3ef1b1975fd0a5f6c59b
arkadiusz-fratczak <arkadiusz.ef@gmail.com>
2017-08-16 22:53:31 +0200

    Solution: hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,6 +1,32 @@
 package hamming
 
+import "fmt"
+
 const testVersion = 6
 
+type DNAStrandsLenghtDiffer struct {
+	Strand1, Strand2 string
+}
+
+func (e *DNAStrandsLenghtDiffer) Error() string {
+	return fmt.Sprintf("Lenght of %s and %s differs: %d != %d", e.Strand1, e.Strand2, len(e.Strand1), len(e.Strand2))
+}
+
+func newDNAStrandsLenghtDiffer(s1, s2 string) *DNAStrandsLenghtDiffer {
+	return &DNAStrandsLenghtDiffer{s1, s2}
+}
+
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, newDNAStrandsLenghtDiffer(a, b)
+	}
+	distance := 0
+	aRunes := []rune(a)
+	bRunes := []rune(b)
+	for i, v := range aRunes {
+		if v != bRunes[i] {
+			distance++
+		}
+	}
+	return distance, nil
 }
