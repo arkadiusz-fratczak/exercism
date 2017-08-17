87ecd97144ddd31dd8b985bf12bfa96db7cf62d1
arkadiusz-fratczak <arkadiusz.ef@gmail.com>
2017-08-17 22:56:21 +0200

    Solution: acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,5 +1,22 @@
 package acronym
 
+import (
+	"unicode"
+)
+
 const testVersion = 3
 
-func Abbreviate(string) string
+func Abbreviate(longName string) string {
+	longNameRunes := []rune(longName)
+	result := []rune{longNameRunes[0]}
+	for i, v := range []rune(longName) {
+		if isSpaceOrDash(v) && i+1 < len(longNameRunes) {
+			result = append(result, unicode.ToUpper(longNameRunes[i+1]))
+		}
+	}
+	return string(result)
+}
+
+func isSpaceOrDash(v rune) bool {
+	return unicode.IsSpace(v) || v == '-'
+}
