f8e332f69c7e94ac05f77822ac5446b842c65196
arkadiusz-fratczak <arkadiusz.ef@gmail.com>
2017-08-17 22:34:22 +0200

    Solution: accumulate

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -2,4 +2,10 @@ package accumulate
 
 const testVersion = 1
 
-func Accumulate([]string, func(string) string) []string
+func Accumulate(stringSlice []string, f func(string) string) []string {
+	result := make([]string, len(stringSlice))
+	for i, s := range stringSlice {
+		result[i] = f(s)
+	}
+	return result
+}
