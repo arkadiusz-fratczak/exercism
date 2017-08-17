768b406b2154db6027f04fa1ae8e75421a70a493
arkadiusz-fratczak <arkadiusz.ef@gmail.com>
2017-08-17 22:05:37 +0200

    Solution: raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,8 +1,23 @@
 package raindrops
 
+import "strconv"
+
 const testVersion = 3
 
-func Convert(int) string
+func Convert(n int) string {
+	result := ""
+	if n % 3 == 0 {
+		result += "Pling"
+	}
+	if n % 5 == 0 {
+		result += "Plang"
+	}
+	if n % 7 == 0 {
+		result += "Plong"
+	}
+	if len(result) == 0 {
+		result += strconv.Itoa(n)
+	}
+	return result
+}
 
-// Don't forget the test program has a benchmark too.
-// How fast does your Convert convert?
