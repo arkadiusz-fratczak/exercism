b19d34b21f152c873d503a4f7761c1e5603d8822
arkadiusz-fratczak <arkadiusz.ef@gmail.com>
2017-09-21 22:20:30 +0200

    Solution: bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,3 +1,46 @@
 package bob
 
+import (
+	"strings"
+	"unicode"
+)
+
 const testVersion = 3
+
+func Hey(sentence string) string {
+	if containsOnlySpaces(sentence) {
+		return "Fine. Be that way!"
+	} else if containsLetters(sentence) && containsOnlyUpperLetters(sentence) {
+		return "Whoa, chill out!"
+	} else if strings.HasSuffix(strings.TrimSpace(sentence), "?") {
+		return "Sure."
+	}
+	return "Whatever."
+}
+
+func containsOnlySpaces(sentence string) bool {
+	for _, r := range sentence {
+		if !unicode.IsSpace(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func containsLetters(sentence string) bool {
+	for _, r := range sentence {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsOnlyUpperLetters(sentence string) bool {
+	for _, r := range sentence {
+		if unicode.IsLetter(r) && unicode.IsLower(r) {
+			return false
+		}
+	}
+	return true
+}
